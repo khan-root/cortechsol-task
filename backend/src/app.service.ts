8c59d07129afe53a99080de17aecb2f0a230a362
Sarmad Faizan <sarmadf49@gmail.com>
2025-07-18 14:14:28 +0500

    connect backend to PG throught prisma

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './config/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Health Checking';
+  constructor(private readonly prisma: PrismaService) {}
+  async getHello(): Promise<string> {
+    try {
+      const users = await this.prisma.user.findMany({ take: 1 });
+      console.log(users);
+      return `Database connected. Users table found`;
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'Unknown error';
+      return `Database connection failed: ${errorMessage}`;
+    }
   }
 }
