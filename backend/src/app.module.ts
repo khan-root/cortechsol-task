c5bd494de6391d20f30f7091dde90306fd9964a0
Sarmad Faizan <sarmadf49@gmail.com>
2025-07-18 14:21:13 +0500

    implement api prefix and create different modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './config/prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
+    UsersModule,
+    TasksModule,
+    UploadModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
